9ec774f03d917f45bc95de31ca538ca46e4baa80
Emmanuel <opensoftweb@gmail.com>
2024-11-06 10:35:15 +0100

    feat: implement handler to index Bullish events

--- ethereum/src/EventHandlers.ts
+++ ethereum/src/EventHandlers.ts
@@ -41,3 +41,45 @@ PredictionMarket.Bearish.handler(async ({ event, context }) => {
     });
   }
 });
+
+PredictionMarket.Bullish.handler(async ({ event, context }) => {
+  let roundId = event.chainId
+    .toString()
+    .concat("#")
+    .concat(
+      event.params.id
+        .toString()
+        .concat("#")
+        .concat(event.params.roundId.toString())
+    )
+    .toLowerCase();
+  let positionId = event.chainId
+    .toString()
+    .concat("#")
+    .concat(event.params.positionId.toString())
+    .toLowerCase();
+
+  let round = await context.Round.get(roundId);
+  let position = await context.Position.get(positionId);
+
+  if (round !== undefined && position === undefined) {
+    context.Position.set({
+      id: positionId,
+      chainId: BigInt(event.chainId),
+      account: event.params.account.toString().toLowerCase(),
+      createdAt: BigInt(event.block.timestamp),
+      claimed: false,
+      market_id: round.market_id,
+      option: "BULLISH",
+      reward: BigInt(0),
+      round_id: roundId,
+      share: event.params.stake,
+    });
+
+    context.Round.set({
+      ...round,
+      bullishShares: round.bullishShares + event.params.stake,
+      totalShares: round.totalShares + event.params.stake,
+    });
+  }
+});
