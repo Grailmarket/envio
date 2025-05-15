01c037ce1212d76b138e3497c42de5df4475e5c3
Emmanuel <opensoftweb@gmail.com>
2025-05-15 18:19:38 +0100

    fix: vaa parsing error

--- ethereum/src/getRoundState.ts
+++ ethereum/src/getRoundState.ts
@@ -5,7 +5,8 @@ const EMITTER_ADDRESS = "0xDD1b2F2D4D04eeb992225F54ca174FD2B0E46a5D";
 
 export async function getRoundState(sequenceId: number) {
     const QUERY_URL = `https://api.wormholescan.io/api/v1/vaas/${EMITTER_CHAIN_ID}/${EMITTER_ADDRESS}/${sequenceId}`
-    const data = await axios.get<{ data: { vaa: string[] }[] }>(QUERY_URL);
+    const response = await axios.get<{ data: { vaa: string } }>(QUERY_URL);
+    const data = Buffer.from(response.data.data.vaa, "base64");
 
-    return data.data.data[0].vaa[0];
+    return data.toString("hex");
 }
